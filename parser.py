da0d1e68580ff0fcf8ef52a2a07bccda11d71b7c
zrehan-1013 <zrehan@terpmail.umd.edu>
2026-04-17 16:48:04 -0400

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,47 @@
+"""
+Docstring for parser, Parses functions
+
+This module is responsible for readin the log file and convertiing each line
+into LogEntry objects. It sepereates input and the parsing to have a seperation
+to make it easier to read
+"""
+
+from log_model import LogEntry
+
+class LogParser:
+    """
+    Parse raw log lines into LogEntry objects.
+    """
+
+    def parse_line(self, line):
+        """
+        Parase raw log lines into LogEntry objects.
+        
+        Expected formate:
+        timestamp, ip_addr, username, status
+        
+        Example:
+        2026-04-10 09:15:00,192.168.1.7,admin,FAIL
+
+        Args:
+        line: One raw line from log file
+
+        """
+
+        parts = line.strip.split(",")
+
+def parse_file(self, filename):
+    """
+    Read a log file and parse each line into the logentry objects
+    
+    :param filename: The name of the file to parse
+    """
+
+    entries = []
+
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            if line.strip:
+                entries.append(self.parse_line(line))
+
+    return entries
