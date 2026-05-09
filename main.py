91841dc904f2ebfbcaa238eadc3ef5e4056f6e75
zrehan-1013 <zrehan@terpmail.umd.edu>
2026-05-08 20:05:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
+"""
+Main file for cybersecurity log analyzer.
+"""
 import sys
-from parser import  LogParser
+from parser import LogParser
 from analyzer import Analyzer
 from repot import ReportGenerator
 
 def main():
+    """
+    Run the cybersecurity log analyzer.
+    """
+
     if len(sys.argv) != 2:
-        print("Usage: python main.py log.txt")
+        print("Usage: python3 main.py log.txt")
         return
-    
+
     filename = sys.argv[1]
 
     parser = LogParser()
     analyzer = Analyzer()
     reporter = ReportGenerator()
 
-    entries = parser.parse_file(filename)
-    total_failed = analyzer.count_failed_logins(entries)
-    suspicious = analyzer.detect_suspicious_ips(entries)
+    try:
+        entries = parser.parse_file(filename)
 
-    report = reporter.generate_report(total_failed, suspicious)
+    except FileNotFoundError:
+        print("Error: File not found.")
+        return
+
+    total_failed = analyzer.count_failed_logins(entries)
+
+    total_successful = analyzer.count_successful_logins(
+        entries
+    )
+
+    suspicious_ips = analyzer.detect_suspicious_ips(
+        entries
+    )
+
+    report = reporter.generate_report(
+        total_failed,
+        total_successful,
+        suspicious_ips
+    )
+
     print(report)
+
 
 if __name__ == "__main__":
     main()
